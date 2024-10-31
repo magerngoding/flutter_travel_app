4a623ec38cce065f6b351f74169d730e2ed442b5
Hafizh <Hafizh@gmail.com>
2024-10-31 15:47:22 +0700

    Membuat top destination sebagian

--- lib/features/destination/presentation/pages/home_page.dart
+++ lib/features/destination/presentation/pages/home_page.dart
@@ -1,7 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_travel/api/urls.dart';
+import 'package:flutter_travel/features/destination/domain/entities/destination_entity.dart';
+import 'package:flutter_travel/features/destination/presentation/bloc/top_destination/top_destination_bloc.dart';
+import 'package:flutter_travel/features/destination/presentation/widgets/circle_loading.dart';
+import 'package:flutter_travel/features/destination/presentation/widgets/text_failure.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,21 +18,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final topDestinationController = PageController();
+  refresh() {
+    context.read<TopDestinationBloc>().add(OnGetTopDestination());
+  }
+
+  @override
+  void initState() {
+    refresh();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        const SizedBox(height: 30.0),
-        header(),
-        const SizedBox(height: 20.0),
-        search(),
-        const SizedBox(height: 24.0),
-        category(),
-        const SizedBox(height: 20.0),
-        topDestination(),
-        const SizedBox(height: 30.0),
-        allDestination(),
-      ],
+    return RefreshIndicator(
+      onRefresh: () async => refresh(),
+      child: ListView(
+        children: [
+          const SizedBox(height: 30.0),
+          header(),
+          const SizedBox(height: 20.0),
+          search(),
+          const SizedBox(height: 24.0),
+          category(),
+          const SizedBox(height: 20.0),
+          topDestination(),
+          const SizedBox(height: 30.0),
+          allDestination(),
+        ],
+      ),
     );
   }
 
@@ -159,7 +180,124 @@ class _HomePageState extends State<HomePage> {
   }
 
   topDestination() {
-    return SizedBox();
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Top Destination",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              BlocBuilder<TopDestinationBloc, TopDestinationState>(
+                builder: (context, state) {
+                  if (state is TopDestinationLoaded) {
+                    return SmoothPageIndicator(
+                      controller: topDestinationController,
+                      count: state.data.length,
+                      effect: WormEffect(
+                        dotColor: Colors.grey[300]!,
+                        activeDotColor: Theme.of(context).primaryColor,
+                        dotHeight: 10,
+                        dotWidth: 10,
+                      ),
+                    );
+                  }
+                  return SizedBox();
+                },
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 16.0,
+        ),
+        BlocBuilder<TopDestinationBloc, TopDestinationState>(
+          builder: (context, state) {
+            if (state is TopDestinationLoading) {
+              return CircleLoading();
+            }
+            if (state is TopDestinationFailure) {
+              return TextFailure(message: state.message);
+            }
+            if (state is TopDestinationLoaded) {
+              List<DestinationEntity> list = state.data;
+              return AspectRatio(
+                aspectRatio: 1.5,
+                child: PageView.builder(
+                  controller: topDestinationController,
+                  itemCount: list.length,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    DestinationEntity destination = list[index];
+                    return itemTopDestination(destination);
+                  },
+                ),
+              );
+            }
+            return SizedBox(
+              height: 120,
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget itemTopDestination(DestinationEntity destination) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+              child: ExtendedImage.network(
+                URLs.image(destination.cover),
+                fit: BoxFit.cover,
+                handleLoadingProgress: true,
+                loadStateChanged: (state) {
+                  if (state.extendedImageLoadState == LoadState.failed) {
+                    return AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Material(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                        color: Colors.grey[300],
+                        child: const Icon(
+                          Icons.broken_image,
+                          color: Colors.black,
+                        ),
+                      ),
+                    );
+                  }
+                  if (state.extendedImageLoadState == LoadState.loading) {
+                    return AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Material(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                        color: Colors.grey[300],
+                        child: CircleLoading(),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   allDestination() {
